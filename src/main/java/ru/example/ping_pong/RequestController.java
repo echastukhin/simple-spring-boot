e798b889e48aca12071019a059f2510531b5c763
echastukhin <ftpclient@rambler.ru>
2019-08-19 20:24:22 +0300

    add LocalDate.now and LocalTime.now to sayDate function

--- src/main/java/ru/example/ping_pong/RequestController.java
+++ src/main/java/ru/example/ping_pong/RequestController.java
@@ -4,12 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @RestController
 public class RequestController {
 
     @RequestMapping("/ping")
     public String sayPong(){
         return "pong...";
+    }
+
+    @RequestMapping("/date")
+    public String sayDate(){
+        return "Date: " + LocalDate.now() +" "+ LocalTime.now();
     }
 
     @Value("${spring.application.name}")
